727e372030771b7771dd2b2f45dfb49aacc83523
Paul Chen <paulchenrox3@gmail.com>
2022-06-22 17:42:30 -0400

    edit urls

--- backend/hang_backend/accounts/urls.py
+++ backend/hang_backend/accounts/urls.py
@@ -3,9 +3,9 @@ from .api import LoginAPI, RegisterAPI, UserAPI
 from knox import views as knox_views
 
 urlpatterns = [
-    path('api/auth', include('knox.urls')),
-    path('api/auth/register', RegisterAPI.as_view()),
-    path('api/auth/login', LoginAPI.as_view()),
-    path('api/auth/user', UserAPI.as_view()),
-    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
+    path('auth', include('knox.urls')),
+    path('auth/register', RegisterAPI.as_view()),
+    path('auth/login', LoginAPI.as_view()),
+    path('auth/user', UserAPI.as_view()),
+    path('auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000'});
+const API = axios.create({ baseURL: 'http://localhost:8000' });
 
 API.interceptors.request.use((req) => {
-    if(localStorage.getItem('profile')){
+    if (localStorage.getItem('profile')) {
         req.headers.Authorization = `Token ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
 
     return req;
 });
 
-export const login = (inputs) => API.post('/api/auth/login', inputs);
-export const signin = (inputs) => API.post('/api/auth/register', inputs);
-export const logout = (token) => API.post('/api/auth/logout', null);
+export const login = (inputs) => API.post('/auth/login', inputs);
+export const signin = (inputs) => API.post('/auth/register', inputs);
+export const logout = (token) => API.post('/auth/logout', null);
