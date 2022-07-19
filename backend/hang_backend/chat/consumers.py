6f3001add10c44f62544047708a6ef4679b1589a
Paul Chen <paulchenrox3@gmail.com>
2022-07-18 23:37:17 -0400

    organize and add error messages to chat; Todo: write documentation

--- backend/hang_backend/chat/consumers.py
+++ backend/hang_backend/chat/consumers.py
@@ -6,7 +6,8 @@ from channels.generic.websocket import WebsocketConsumer
 from django.contrib.auth.models import User
 
 from .models import Message, MessageChannel
-from .serializers import MessageSerializer, SendMessageSerializer
+from .serializers import MessageSerializer, SendMessageSerializer, LoadMessageSerializer, EditMessageSerializer, \
+    DeleteMessageSerializer
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -18,7 +19,6 @@ class ChatConsumer(WebsocketConsumer):
 
         self.user = User.objects.get(username=username)
 
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.user.username,
             self.channel_name
@@ -27,145 +27,149 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.user.username,
             self.channel_name
         )
 
-    # Receive message from WebSocket
     def receive(self, text_data=None, bytes_data=None):
         try:
             text_data_json = json.loads(text_data)
             text_data_json['user'] = {'username': self.user.username}
 
             if text_data_json['type'] == 'send':
-                serializer = SendMessageSerializer(data=text_data_json)
-                serializer.is_valid()
-
-                if not serializer.is_valid():
-                    self.error(json.loads(json.dumps(serializer.errors)))
-                    return
-
-
-                message = serializer.save()
-
-                self.success()
-
-                for e in message.message_channel.users.all():
-                    async_to_sync(self.channel_layer.group_send)(
-                        e.username,
-                        {
-                            'type': 'receive_message',
-                            'message': MessageSerializer(message).data,
-                        }
-                    )
+                self.send_message(text_data_json)
             elif text_data_json['type'] == 'load':
-                channel = text_data_json['channel']
-                before = text_data_json.get('before', Message.objects.latest('id').id + 1000000000)
-
-                message_channel = self.get_message_channel(channel)
-                if message_channel is None:
-                    return
-
-                messages = message_channel.message_set.all().filter(
-                    id__lte=before).order_by('-id')
-
-                msg_list = []
-                for e in messages[:min(20, len(messages))]:
-                    msg_list.append(MessageSerializer(e).data)
-
-                async_to_sync(self.channel_layer.send)(
-                    self.channel_name,
-                    {
-                        'type': 'load_message',
-                        'messages': msg_list,
-                    }
-                )
-
-                self.success()
+                self.load_message(text_data_json)
             elif text_data_json['type'] == 'edit':
-                message_id = text_data_json['message_id']
-                new_message = text_data_json['new_message']
-
-                message = self.get_message(message_id)
-                if message is None:
-                    return
-
-                message_channel_id = message.message_channel_id
-
-                if message.user.id != self.user.id:
-                    self.error('cannot modify another user\'s message')
-                message.content = new_message
-                message.save()
-
-                message_channel = self.get_message_channel(message_channel_id)
-                if message_channel is None:
-                    return
-
-                self.success()
-
-                for e in message_channel.users.all():
-                    async_to_sync(self.channel_layer.group_send)(
-                        e.username,
-                        {
-                            'type': 'edit_message',
-                            'message': MessageSerializer(message).data
-                        }
-                    )
+                self.edit_message(text_data_json)
             elif text_data_json['type'] == 'delete':
-                message_id = text_data_json['message_id']
-
-                message = self.get_message(message_id)
-                if message is None:
-                    return
-
-                message_channel_id = message.message_channel_id
-
-                if message.user.id != self.user.id:
-                    self.error('cannot modify another user\'s message')
-                    return
-                message.delete()
-
-                message_channel = self.get_message_channel(message_channel_id)
-                if message_channel is None:
-                    return
-
-                self.success()
-
-                for e in message_channel.users.all():
-                    async_to_sync(self.channel_layer.group_send)(
-                        e.username,
-                        {
-                            'type': 'delete_message',
-                            'id': message_id,
-                        }
-                    )
+                self.delete_message(text_data_json)
 
         except (json.JSONDecodeError, KeyError):
-            self.error('invalid json')
+            self.error('Invalid json.')
+        except (Exception,) as e:
+            self.error('Internal server error.')
 
-    def get_message_channel(self, channel_id):
-        try:
-            message_channel = MessageChannel.objects.get(id=channel_id)
-            if not message_channel.users.filter(username=self.user.username).exists():
-                raise MessageChannel.DoesNotExist()
-            return message_channel
-        except MessageChannel.DoesNotExist:
-            self.error('message channel does not exist')
-        except MessageChannel.MultipleObjectsReturned:
-            self.error('internal server error')
-        return None
+    def send_message(self, data):
+        serializer = SendMessageSerializer(data=data)
+        serializer.is_valid()
 
-    def get_message(self, message_id):
-        try:
-            message = Message.objects.get(id=message_id)
-            if message.user.id != self.user.id:
-                raise Message.DoesNotExist()
-            return message
-        except Message.DoesNotExist:
-            self.error('message does not exist')
-        return None
+        if not serializer.is_valid():
+            self.error(json.loads(json.dumps(serializer.errors)))
+            return
+
+        message = serializer.save()
+
+        self.success()
+
+        for e in message.message_channel.users.all():
+            async_to_sync(self.channel_layer.group_send)(
+                e.username,
+                {
+                    'type': 'action',
+                    'action': 'send_message',
+                    'content': {
+                        'message': MessageSerializer(message).data,
+                    },
+                }
+            )
+
+    def load_message(self, data):
+        serializer = LoadMessageSerializer(data=data)
+        serializer.is_valid()
+
+        if not serializer.is_valid():
+            self.error(json.loads(json.dumps(serializer.errors)))
+            return
+
+        message_channel = serializer.validated_data['message_channel']
+        message_id = serializer.validated_data['message_id']
+
+        if message_id is None:
+            messages = message_channel.message_set.all().order_by('-id')
+        else:
+            messages = message_channel.message_set.all().filter(
+                id__lte=message_id).order_by('-id')
+
+        msg_list = [MessageSerializer(e).data for e in messages[:min(20, len(messages))]]
+
+        self.success()
+
+        async_to_sync(self.channel_layer.send)(
+            self.channel_name,
+            {
+                'type': 'action',
+                'action': 'load_message',
+                'content': {
+                    'messages': msg_list,
+                }
+            }
+        )
+
+    def edit_message(self, data):
+        serializer = EditMessageSerializer(data=data)
+        serializer.is_valid()
+
+        if not serializer.is_valid():
+            self.error(json.loads(json.dumps(serializer.errors)))
+            return
+
+        message_id = serializer.validated_data['message_id']
+        content = serializer.validated_data['content']
+        message = Message.objects.get(id=message_id)
+
+        message_channel_id = message.message_channel_id
+
+        message.content = content
+        message.save()
+
+        self.success()
+
+        for e in MessageChannel.objects.get(id=message_channel_id).users.all():
+            async_to_sync(self.channel_layer.group_send)(
+                e.username,
+                {
+                    'type': 'action',
+                    'action': 'edit_message',
+                    'content': {
+                        'message': MessageSerializer(message).data
+                    }
+                }
+            )
+
+    def delete_message(self, data):
+        serializer = DeleteMessageSerializer(data=data)
+        serializer.is_valid()
+
+        if not serializer.is_valid():
+            self.error(json.loads(json.dumps(serializer.errors)))
+            return
+
+        message = Message.objects.get(id=serializer.validated_data['message_id'])
+        message_channel = message.message_channel
+
+        message.delete()
+
+        self.success()
+
+        for e in message_channel.users.all():
+            async_to_sync(self.channel_layer.group_send)(
+                e.username,
+                {
+                    'type': 'action',
+                    'action': 'delete_message',
+                    'content': {
+                        'id': serializer.validated_data['message_id'],
+                    }
+                }
+            )
+
+    def status(self, event):
+        self.send(text_data=json.dumps({
+            'type': 'status',
+            'message': event['message'],
+        }))
 
     def success(self):
         async_to_sync(self.channel_layer.send)(
@@ -185,33 +189,8 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    def status(self, event):
+    def action(self, event):
         self.send(text_data=json.dumps({
-            'type': 'status',
-            'message': event['message'],
-        }))
-
-    def receive_message(self, event):
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'type': 'receive_message',
-            'message': event['message'],
-        }))
-
-    def load_message(self, event):
-        self.send(text_data=json.dumps({
-            'type': 'load_message',
-            'messages': event['messages'],
-        }))
-
-    def edit_message(self, event):
-        self.send(text_data=json.dumps({
-            'type': 'edit_message',
-            'message': event['message'],
-        }))
-
-    def delete_message(self, event):
-        self.send(text_data=json.dumps({
-            'type': 'delete_message',
-            'id': event['id'],
+            'action': event['action'],
+            'content': event['content'],
         }))
--- backend/hang_backend/chat/serializers.py
+++ backend/hang_backend/chat/serializers.py
@@ -5,18 +5,53 @@ from .models import Message, MessageChannel
 
 
 class UserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField()
+    id = serializers.IntegerField(default=None)
+    username = serializers.CharField(default=None)
+    email = serializers.EmailField(default=None)
 
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
 
+    def validate_id(self, data):
+        if data is not None and not User.objects.filter(id=data).exists():
+            raise serializers.ValidationError("The user does not exist.")
+        return data
+
+    def validate_username(self, data):
+        if data is not None and not User.objects.filter(username=data).exists():
+            raise serializers.ValidationError("The user does not exist.")
+        return data
+
+    def validate_email(self, data):
+        if data is not None and not User.objects.filter(email=data).exists():
+            raise serializers.ValidationError("The user does not exist.")
+        return data
+
     def validate(self, data):
-        return User.objects.get(username=data['username'])
+        if data['id'] is not None:
+            return User.objects.get(id=data['id'])
+        if data['username'] is not None:
+            return User.objects.get(username=data['username'])
+        if data['email'] is not None:
+            return User.objects.get(email=data['email'])
+        raise serializers.ValidationError("One of id, username, and email must be present.")
 
 
 class MessageChannelSerializer(serializers.Serializer):
     id = serializers.CharField(max_length=10)
+
+    class Meta:
+        model = MessageChannel
+        fields = ('id')
+
+    def validate_id(self, data):
+        if not MessageChannel.objects.filter(id=data).exists():
+            raise serializers.ValidationError("The message channel does not exist.")
+        return data
+
+    def validate(self, data):
+        return MessageChannel.objects.get(id=data['id'])
 
 
 class MessageSerializer(serializers.Serializer):
@@ -27,44 +62,55 @@ class MessageSerializer(serializers.Serializer):
     content = serializers.CharField(max_length=2000)
     message_channel = MessageChannelSerializer()
 
-    def create(self, validated_data):
-        return Message.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.id = validated_data.get('id', instance.id)
-        instance.user = validated_data.get('user', instance.user)
-        instance.created_at = validated_data.get('created_at', instance.created)
-        instance.updated_at = validated_data.get('updated_at', instance.updated_at)
-        instance.content = validated_data.get('content', instance.content)
-        instance.message_channel = validated_data.get('message_channel', instance.message_channel)
-        instance.save()
-        return instance
-
 
 class CreateDMSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    user = UserSerializer()
 
-    def validate_email(self, data):
-        if not User.objects.filter(email=data).exists():
-            raise serializers.ValidationError("The email is not associated with an account.")
+    def validate(self, data):
+        return User.objects.get(id=data['user'].id)
+
+
+class SendMessageSerializer(serializers.Serializer):
+    user = UserSerializer()
+    message_channel = MessageChannelSerializer()
+    content = serializers.CharField()
+
+    def validate_content(self, data):
+        if len(data) > 2000:
+            raise serializers.ValidationError("Messages cannot be over 2000 characters long.")
         return data
 
     def validate(self, data):
-        return User.objects.get(email=data['email'])
+        if not data['message_channel'].users.filter(username=data['user'].username).exists():
+            raise serializers.ValidationError("Permission denied.")
+        return data
+
+    def create(self, data):
+        message = Message(user=data['user'], content=data['content'],
+                          message_channel=data['message_channel'])
+        message.save()
+        return message
 
 
-class SendMessageSerializer(serializers.ModelSerializer):
+class LoadMessageSerializer(serializers.Serializer):
     user = UserSerializer()
-    message_channel = serializers.CharField()
+    message_channel = MessageChannelSerializer()
+    message_id = serializers.IntegerField(default=None)
+
+    def validate(self, data):
+        if not data['message_channel'].users.filter(username=data['user'].username).exists():
+            raise serializers.ValidationError("Permission denied.")
+        return data
+
+
+class EditMessageSerializer(serializers.Serializer):
+    user = UserSerializer()
+    message_id = serializers.IntegerField()
     content = serializers.CharField()
 
-    class Meta:
-        model = Message
-        fields = ('user', 'message_channel', 'content')
-
-    def validate_message_channel(self, data):
-        if not MessageChannel.objects.filter(id=data).exists():
-            raise serializers.ValidationError("The message channel does not exist.")
+    def validate_message_id(self, data):
+        if not Message.objects.filter(id=data).exists():
+            raise serializers.ValidationError("The message does not exist.")
         return data
 
     def validate_content(self, data):
@@ -73,13 +119,21 @@ class SendMessageSerializer(serializers.ModelSerializer):
         return data
 
     def validate(self, data):
-        if not MessageChannel.objects.filter(id=data['message_channel']).get().users.filter(
-                username=data['user'].username).exists():
+        if not Message.objects.filter(id=data["message_id"]).get().user.username == data["user"].username:
             raise serializers.ValidationError("Permission denied.")
         return data
 
-    def create(self, data):
-        message = Message(user=data['user'], content=data['content'],
-                          message_channel=MessageChannel.objects.get(id=data['message_channel']))
-        message.save()
-        return message
+
+class DeleteMessageSerializer(serializers.Serializer):
+    user = UserSerializer()
+    message_id = serializers.IntegerField()
+
+    def validate_message_id(self, data):
+        if not Message.objects.filter(id=data).exists():
+            raise serializers.ValidationError("The message does not exist.")
+        return data
+
+    def validate(self, data):
+        if not Message.objects.filter(id=data["message_id"]).get().user.username == data["user"].username:
+            raise serializers.ValidationError("Permission denied.")
+        return data
