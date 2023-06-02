4cfce6182fd56558b1f53b2f381658d0c298b17e
Eeetan <ethanqiu@gmail.com>
2023-06-02 13:47:12 -0400

    Fixed Join Link

--- frontend/src/components/Hang/Hangs/Join/Join.js
+++ frontend/src/components/Hang/Hangs/Join/Join.js
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {joinhangevent} from "../../../../actions/hang";
 import {useDispatch} from "react-redux";
+import {FRONTENDURL} from "../../../../constants/actionTypes";
 
 const Join = () => {
 
@@ -17,6 +18,9 @@ const Join = () => {
     useEffect(() => {
         const code = getCode();
         dispatch(joinhangevent(code, navigate)).then((r) =>{
+            if(r !== undefined){
+                window.location.href = `${FRONTENDURL}hang/room?id=${r.id}`
+            }
             console.log(r);
         });
     }, []);
