b4851fa9da6b3ad54e154865dc9103f477e1d370
Eeetan <ethanqiu@gmail.com>
2023-06-08 18:26:19 -0400

    Chat UI fixes

--- frontend/src/components/Chat/Chatroom/Form/Form.js
+++ frontend/src/components/Chat/Chatroom/Form/Form.js
@@ -58,7 +58,7 @@ const Form = ({ client, currentRoom }) => {
     }, [useSelector((state) => state.users), currentRoom])
 
     return(
-        <Box sx={{height: "100%", display: "flex", borderRadius: "0 0 10px 0", alignItems: "center"}}>
+        <Box sx={{height: "100%", width: "100%", display: "flex", borderRadius: "0 0 10px 0", alignItems: "center"}}>
             {
                 JSON.parse(localStorage.getItem('profile') !== null) &&
                 <form onSubmit={messageSend}>
